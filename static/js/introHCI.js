61d0ff2d5d062614535d2b6986e866541d112d78
Will0796 <Will0796@hotmail.com>
2019-01-24 22:01:01 -0800

    123

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,17 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color","#7fff00");
+	var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
